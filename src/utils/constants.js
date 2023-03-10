8e0af927b5227135c5b9c20208b528b6f3b05470
SomnathKar000 <somnathkar201901@gmail.com>
2023-03-10 17:45:24 +0530

    Change into localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -39,10 +39,10 @@ export const services = [
   },
 ];
 
-// export const products_url = "http://localhost:5000/api/v1/store-products";
-export const products_url = "http://54.211.26.216:5000/api/v1/store-products";
-// export const single_product_url = `http://localhost:5000/api/v1/store-single-products?id=`;
-export const single_product_url = `http://54.211.26.216:5000/api/v1/store-single-products?id=`;
+export const products_url = "http://localhost:5000/api/v1/store-products";
+// export const products_url = "http://54.211.26.216:5000/api/v1/store-products";
+export const single_product_url = `http://localhost:5000/api/v1/store-single-products?id=`;
+// export const single_product_url = `http://54.211.26.216:5000/api/v1/store-single-products?id=`;
 
 export const extraImages = [
   {

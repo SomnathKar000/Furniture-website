91a30ec0e785aebd4eaf4607b7e8e506c0f4619b
SomnathKar000 <somnathkar201901@gmail.com>
2023-03-16 22:59:11 +0530

    Updating orderlist

--- backend/controllers/orderListControler.js
+++ backend/controllers/orderListControler.js
@@ -1,7 +1,9 @@
 const Order = require("../models/orderModule");
 
-const getAllOrderList = (req, res) => {
-  res.status(200).send({ success: true, msg: "done" });
+const getAllOrderList = async (req, res) => {
+  const userId = req.user.id;
+  const data = await Order.find({ userId: userId });
+  res.status(200).send({ success: true, data });
 };
 
 module.exports = { getAllOrderList };
